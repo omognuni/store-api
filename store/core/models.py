7d133dc1019d4ca5ac2051adb08925e53f37bdfd
omognuni <melbong0069@gmail.com>
2022-10-27 10:46:56 +0900

    [Add] 모델 구현

--- store/core/models.py
+++ store/core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from enum import Enum
+
+class Status(str, Enum):
+    PENDING = 'pending'
+    COMPLETE = 'complete'
+    
+    @classmethod
+    def choices(cls):
+        return tuple((x.value, x.name) for x in cls)
+    
+    def __str__(self):
+        return self.value
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    price = models.IntegerField()
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    item = models.ForeignKey('Item', on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=255,choices=Status.choices())
+    
+    def __str__(self):
+        return self.status
