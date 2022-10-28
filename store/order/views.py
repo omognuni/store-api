defe9f0f4910a5d2f73f0313d3b2c55122593b1a
omognuni <melbong0069@gmail.com>
2022-10-28 16:49:51 +0900

    [Feat] 이용자, 관리자 주문 목록 분리 구현

--- store/order/views.py
+++ store/order/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+
 from core.models import Order
 
 from order.permissions import IsAdminOrCRUOnly
@@ -15,3 +15,11 @@ class OrderViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            queryset = self.queryset
+        else:
+            queryset = self.queryset.filter(user=self.request.user.id)
+        return queryset
+            
