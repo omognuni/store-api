c33ee004f37c11182d09a666cc84434be3a83183
omognuni <melbong0069@gmail.com>
2022-10-28 17:15:15 +0900

    [Feat] Api Swagger 적용

--- store/store/settings.py
+++ store/store/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_spectacular',
     'core',
     'user',
     'item',
@@ -139,4 +140,5 @@ REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
        'rest_framework.authentication.TokenAuthentication',
    ),
+   'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
--- store/store/urls.py
+++ store/store/urls.py
@@ -1,3 +1,8 @@
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +11,10 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/item/', include('item.urls')),
     path('api/order/', include('order.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
+        name='api-docs',
+    )
 ]
